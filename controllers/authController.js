07e4837042737a349e6ef90a1e3b71927535dab2
Jashan <jashandeep4329@gmail.com>
2025-03-09 23:27:45 +0530

    Updated profile update functionality and fixed errors

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,93 +3,116 @@ require("dotenv").config();
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
 
-
 const userModel = require("../database/db");
-const signupSchema = require ("../validations/authSchemas");
+const signupSchema = require("../validations/authSchemas");
 
-const  userVerification  = require("../middlewares/userAuth");
-
-
-
-
+const userVerification = require("../middlewares/userAuth");
 
 exports.signup = async (req, res) => {
-    try{
+  try {
     console.log("Received body:", req.body);
 
-   const validation = signupSchema.safeParse(req.body);
+    const validation = signupSchema.safeParse(req.body);
 
-   if(!validation.success){
-    return res.status(400).json({message : validation.errors.error});
-   }
+    if (!validation.success) {
+      return res.status(400).json({ message: validation.errors.error });
+    }
 
-    const {username,email,password} = (req.body);
-    
-    const hashedPassword = await bcrypt.hash(password,5);
+    const { username, email, password } = req.body;
 
-   await userModel.create({
-       username,
-       email,
-       password : hashedPassword,
-       isDriver : false
+    const hashedPassword = await bcrypt.hash(password, 5);
 
-   })
-    res.json({message : "the user is added"});
-} catch (error){
-    console.error("signup error",error);
+    await userModel.create({
+      username,
+      email,
+      password: hashedPassword,
+      isDriver: false,
+    });
+    res.json({ message: "the user is added" });
+  } catch (error) {
+    console.error("signup error", error);
     res.status(500).json({ error: "Something went wrong" });
-}
+  }
 };
 
- 
-exports.signin = async (req,res)=>{
-   
-        console.log(req.body);
+exports.signin = async (req, res) => {
+  console.log(req.body);
 
-    const {username , password} = (req.body);
+  const { username, password } = req.body;
 
-    try{
+  try {
     const foundUser = await userModel.findOne({
-        username : username
-    })
+      username: username,
+    });
 
-    if(!foundUser){
-        return res.status(400).json({message : "this user does not exist"});
-    }else{
-        const pass = await bcrypt.compare(password,foundUser.password);
+    if (!foundUser) {
+      return res.status(400).json({ message: "this user does not exist" });
+    } else {
+      const pass = await bcrypt.compare(password, foundUser.password);
 
-        if(!pass){
-            return res.status(400).json({message : "your password or username is wrong"});
-        }else{
-            const token = await jwt.sign({username : username},process.env.JWT_SECRET);
-            return res.json({
-                message : "you are signed in",
-                token : token
-            });
-        }
+      if (!pass) {
+        return res
+          .status(400)
+          .json({ message: "your password or username is wrong" });
+      } else {
+        const token = await jwt.sign(
+          { username: username },
+          process.env.JWT_SECRET
+        );
+        return res.json({
+          message: "you are signed in",
+          token: token,
+        });
+      }
     }
-}catch (error){
-    res.status(500).json({error: "something went wrong"});
-}
+  } catch (error) {
+    res.status(500).json({ error: "something went wrong" });
+  }
+};
 
-}
+exports.getUserProfile = async (req, res) => {
+  const foundUser = await userModel.findOne({
+    username: req.user.username,
+  });
 
-exports.getUserProfile = async (req,res)=>{
+  if (!foundUser) {
+    return res.status(400).json({ message: "something went wrong" });
+  } else {
+    res.json({
+      username: foundUser.username,
+      email: foundUser.email,
+    });
+  }
+};
 
-    
 
-      const foundUser = await userModel.findOne({
+  
+  exports.updateProfile = async (req, res) => {
+    try {
+      const updates = {};
+  
+      Object.keys(req.body).forEach((key) => {
+        if (req.body[key]) {
+          updates[key] = req.body[key];
+        }
+      });
+  
+      const updatedUser = await userModel.findOneAndUpdate(
+        { username: req.user.username },
+        { $set: updates },
+        { new: true, runValidators: true } 
+      );
+  
+      if (!updatedUser) {
+        return res.status(400).json({ error: "User not found" });
+      }else{
+  
+      return res.json({ message: "Profile updated successfully", updatedUser });
+    }
+  
+    } catch (error) {
+      res.status(500).json({ message: "Internal server error", error: error.message });
+    }
+  };
+  
 
-         username : req.user.username
-       })
-
-       if(!foundUser){
-        return res.status(400).json({message: "something went wrong"});
-       }else {
-        res.json({
-           username : foundUser.username,
-           email: foundUser.email
-        })
-       }
-
-}
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,5 +1,5 @@
 const {Router} = require ("express");
-const {signup,signin,getUserProfile} = require ("../../controllers/authController");
+const {signup,signin,getUserProfile,updateProfile} = require ("../../controllers/authController");
 
 const userVerification = require ('../../middlewares/userAuth');
 
@@ -12,7 +12,7 @@ userRouter.post('/signin',signin);
 
 userRouter.get('/profile', userVerification, getUserProfile);
 
-
+userRouter.patch('/update-profile',userVerification,updateProfile);
 
 module.exports = userRouter;
 
