00cf5333feb3d6ccdecbae7b1ec3cb42ab6f5fb5
Rashi1604 <rashimadan270@gmail.com>
2025-03-22 12:34:46 +0530

    project viewed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+ 
 
   return (
     <>
-     <div className='bg-red-400'>HELLOW</div>
+     <div className='bg-red-400'>HELLO</div>
     </>
   )
 }
